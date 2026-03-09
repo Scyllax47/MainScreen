b83afb95294dafeab7004400ddc12220ee00c702
Scyllax47 <grivas28sept@gmail.com>
2026-03-09 08:33:48 -0700

    Back button added

--- app/src/main/java/com/example/firstapp/MainScreen.kt
+++ app/src/main/java/com/example/firstapp/MainScreen.kt
@@ -206,6 +206,30 @@ fun LoginScreen(navController: NavController) {
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
 
+                Box(
+                    modifier = Modifier
+                        .align (Alignment.Start)
+                        .height(55.dp)
+                        .width(120.dp)
+                        .background(
+                            Color(0xFF5E4AE3),
+                            RoundedCornerShape(30.dp)
+                        )
+                        .clickable {
+                            navController.navigate(Screen.Welcome.route)
+                        }
+                    ,
+                    contentAlignment = Alignment.Center
+                ) {
+
+                    Text(
+                        text = "Back",
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp
+                    )
+                }
+
                 // Imagen superior
                 Image(
                     painter = painterResource(id = R.drawable.login_image),
@@ -296,9 +320,7 @@ fun LoginScreen(navController: NavController) {
                             shape = RoundedCornerShape(30.dp)
                         )
                         .padding(horizontal = 30.dp, vertical = 12.dp)
-                        .clickable {
-                            navController.navigate(Screen.Register.route)
-                        },
+                    ,
                     contentAlignment = Alignment.Center
                 ) {
 
@@ -338,6 +360,29 @@ fun RegisterScreen(navController: NavController) {
                     .padding(30.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
+                Box(
+                    modifier = Modifier
+                        .align (Alignment.Start)
+                        .height(55.dp)
+                        .width(120.dp)
+                        .background(
+                            Color(0xFF5E4AE3),
+                            RoundedCornerShape(30.dp)
+                        )
+                        .clickable {
+                            navController.navigate(Screen.Welcome.route)
+                        }
+                    ,
+                    contentAlignment = Alignment.Center
+                ) {
+
+                    Text(
+                        text = "Back",
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp
+                    )
+                }
 
                 Text(
                     text = "Register",
@@ -416,9 +461,7 @@ fun RegisterScreen(navController: NavController) {
                             Color(0xFF5E4AE3),
                             RoundedCornerShape(30.dp)
                         )
-                        .clickable {
-                            navController.navigate(Screen.Welcome.route)
-                        },
+                        ,
                     contentAlignment = Alignment.Center
                 ) {
 
